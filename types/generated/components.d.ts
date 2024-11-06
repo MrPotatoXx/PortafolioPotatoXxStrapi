dc383900135f18ec4cabff49e6513a5bd7c1791e
MrPotatoXx <salinasgiovanni25@gmail.com>
2024-11-06 18:30:29 -0300

    Modificar los esquemas de trabajo y educación para hacer campos opcionales y agregar descripciones

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,17 +1,4 @@
-import type { Struct, Schema } from '@strapi/strapi';
-
-export interface ProfileLinkProfileLink extends Struct.ComponentSchema {
-  collectionName: 'components_profile_link_profile_link_s';
-  info: {
-    displayName: 'ProfileLink ';
-    icon: 'code';
-  };
-  attributes: {
-    network: Schema.Attribute.String & Schema.Attribute.Required;
-    username: Schema.Attribute.String;
-    url: Schema.Attribute.String & Schema.Attribute.Required;
-  };
-}
+import type { Schema, Struct } from '@strapi/strapi';
 
 export interface LocationLocation extends Struct.ComponentSchema {
   collectionName: 'components_location_locations';
@@ -21,18 +8,31 @@ export interface LocationLocation extends Struct.ComponentSchema {
   };
   attributes: {
     addres: Schema.Attribute.String & Schema.Attribute.Required;
-    postalCode: Schema.Attribute.BigInteger & Schema.Attribute.Required;
     city: Schema.Attribute.String;
     countryCode: Schema.Attribute.String & Schema.Attribute.Required;
+    postalCode: Schema.Attribute.BigInteger & Schema.Attribute.Required;
     region: Schema.Attribute.String;
+  };
+}
+
+export interface ProfileLinkProfileLink extends Struct.ComponentSchema {
+  collectionName: 'components_profile_link_profile_link_s';
+  info: {
+    displayName: 'ProfileLink ';
+    icon: 'code';
+  };
+  attributes: {
+    network: Schema.Attribute.String & Schema.Attribute.Required;
+    url: Schema.Attribute.String & Schema.Attribute.Required;
+    username: Schema.Attribute.String;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'profile-link.profile-link': ProfileLinkProfileLink;
       'location.location': LocationLocation;
+      'profile-link.profile-link': ProfileLinkProfileLink;
     }
   }
 }
